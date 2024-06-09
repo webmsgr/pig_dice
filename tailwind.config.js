e63044a6d68975270f515305dce94344563c1481
webmsgr <zapcontact000@gmail.com>
2024-06-09 01:12:35 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      "dark" // no light mode :)
+    ]
+  }
+}
+
